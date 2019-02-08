1ea211819c96dd20ecaf04eaaf249f498d056704
jw2249a <jw2249a@student.american.edu>
2019-02-08 16:05:14 -0500

    Age Tool, drafting

---
+++ NameMatching/R/name_matching.R
@@ -0,0 +1,39 @@
+
+
+numberOfWords <- function(x) {
+  return(sapply(strsplit(x, " "), length))
+}
+
+nameInitials <- function(x) {
+  substr(x, 0, 1)
+}
+
+
+firstName <- function(x, n = 1) {
+  words = numberOfWords(x)
+  if (words <= n) {
+    return(NA)
+    break()
+  }
+  name = substr(x = x, start = 0, stop = gregexpr(pattern = " ", text = x)[[1]][n]-1)
+  return(name)
+}
+
+lastName <- function(x, n=1) {
+  length = length(charToRaw(x))
+  words = numberOfWords(x)
+  if (words <= n) {
+    return(NA)
+    break()
+  }
+  name = substr(x = x, start = gregexpr(pattern = " ", text = x)[[1]][words-n]+1, stop = length)
+  return(name)
+}
+
+matchNames <- function(x, y, z) {
+  
+}
+
+lapply(nevada_control$Full.Name, firstName, n=1)
+lapply(nevada_control$Full.Name, lastName, n=3)
+
